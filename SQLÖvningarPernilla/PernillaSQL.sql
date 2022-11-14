ad6dd1d9681cc79fb618d47578983a3b809b5ce5
pernillatho <112381808+pernillatho@users.noreply.github.com>
2022-11-14 14:24:27 +0100

    Add files via upload

---
+++ SQLÖvningarPernilla/PernillaSQL.sql
@@ -0,0 +1,35 @@
+	--CREATE DATABASE "DjurDb";
+	--Use DjurDb;
+	--CREATE TABLE DjurTbl (
+	--Id int Primary Key,
+	--Namn nvarchar(max) not null
+	--);
+
+	--INSERT into DjurTbl (Id, Namn)
+	--VALUES (1, 'Hamster'), (2,'Hund'), (3, 'Katt'), (4, 'Kanin');
+
+	--SELECT * FROM DjurTbl;
+
+	--SELECT * FROM DjurTbl WHERE Namn LIKE '%a%';
+
+	--DROP TABLE DjurTbl;
+	
+	--DROP DATABASE DjurDb; 
+
+	--CREATE DATABASE "KlassenDb";
+
+	--CREATE TABLE "Person" (
+	--Id int Primary Key,
+	--Förnamn nvarchar(max) not null, 
+	--Efternamn nvarchar(max) not null, 
+	--Ålder nvarchar(max) not null, 
+	--HemOrt nvarchar(max) not null
+	--);
+	
+	--insert into Person (Id, Förnamn, Efternamn, Ålder, HemOrt)
+	--values (1, 'Pernilla', 'Thorén', 31, 'Kungälv'),
+	--(2, 'Pontus', 'Ekdahl', 27, 'Borås'),
+	--(3, 'Alexander', 'Goold', 28, 'Sidney'),
+	--(4, 'Jacob', 'Fjellström', 27, 'Mölndal');
+
+	--Select * from Person;
